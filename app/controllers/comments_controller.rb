31201b52bc8c9d8b7bc983df00a04f905db1075f
ryo106 <s86.shimauchi.ryosuke@gmail.com>
2020-12-08 17:40:08 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  def create
+    
+    comment = Comment.create(comment_params)
+    redirect_to "/tweets/#{comment.tweet.id}"
+    #アソシエーションはデータベース同士の関係性
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+    # ネストは道筋
+  end
+end
